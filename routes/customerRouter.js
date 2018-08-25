c38f192a07691f5ba8b900698c167b9f40bf6f1c
Brande George <george.brande@gmail.com>
2018-08-25 14:42:30 +0300

    customer OK - toate metodele

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -16,6 +16,49 @@ customerRouter.route('/')
 		console.log(err);
 		res.status(500).json({msg:"error", details:err});
 	});
+})
+.post((req,res)=>{
+	Customer.create({
+		"firstname":req.body.firstname,
+		"lastname":req.body.lastname,
+		"age":req.body.age
+	}).then(customer=>{
+		res.json(customer);
+	}).catch(err=>{
+		console.log(err);
+		res.status(500).json({msg:"error", details:err});
+	});
+})
+.put((req,res)=>{
+	const id = req.body.id;
+	Customer.update( req.body,
+		{where: {id: id}}).then(()=>{
+			res.status(200).json({msg:"Update Successfully -> CUstomer Id ="+ id});
+		}).catch(err=>{
+			console.log(err);
+			res.status(500).json({msg: "error", details:err});
+		});
+});
+
+customerRouter.route('/:id')
+.get((req,res)=>{
+	Customer.findById(req.params.id).then(customer=>{
+		res.json(customer);
+	}).catch(err=>{
+		console.log(err);
+		res.status(500).json({msg:"error",details:err});
+	});
+})
+.delete((req,res)=>{
+	const id = req.params.id;
+	Customer.destroy({
+		where: {id: id }
+	}).then(()=>{
+		res.status(200).json({ msg: "Delete Successfully -> Customer id ="+id});
+	}).catch(err=>{
+		console.log(err);
+		res.status(500).json({msg:"error", details:err});
+	});
 });
 
 
